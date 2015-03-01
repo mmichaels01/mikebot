a2d487caefa9ee0cda6ee6e244e79d0e4014f73d
mmichaels01 <theomegapoint@gmail.com>
2015-03-01 22:12:12 +0000

    Deleted old files

--- MikeBot/TestStream.py
+++
@@ -1,8 +0,0 @@
-import sys
-import time
-import os
-from Stream import Stream
-
-stream=Stream(.2, 60)
-stream.StartStream()
-
--- MikeBot/TestStreamLoop.py
+++
@@ -1,8 +0,0 @@
-import sys
-import time
-import os
-from Stream import Stream
-
-stream=Stream(1, 604800)
-stream.StartStream()
-
